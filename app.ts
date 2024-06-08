dd649036f0b8b8f9d25deb443adc5519075623b8
syeda <sbtshakeel61@gmail.com>
2024-06-08 23:15:29 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+//overloading function
+
+function hello (name:string):string
+function hello (age?:number):number //optional
+function hello(abc :any){
+    return abc;
+}
+hello("hello" )
+hello(12)
+hello("Shakeel")
+
+
+
